862c5e6c05ecd438774ef3c5a8ff29f212c9dd18
Keepers <ryanfkeepers@gmail.com>
2026-04-30 15:15:59 -0600

    try putting the dts under the board

--- boards/arm/rp2040_plus/board.cmake
+++ boards/arm/rp2040_plus/board.cmake
@@ -4,9 +4,3 @@
 board_runner_args(uf2 "--board-id=RPI-RP2")
 
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
-
-# Add repo-local devicetree include root so that:
-# #include <raspberrypi/rpi_pico/rp2040.dtsi>
-# works
-get_filename_component(REPO_ROOT ${CMAKE_CURRENT_LIST_DIR}/../../.. ABSOLUTE)
-list(APPEND DTC_INCLUDE_DIRS ${REPO_ROOT}/dts/arm)
